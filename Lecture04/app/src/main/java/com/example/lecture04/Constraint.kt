c1df039e47b2f1f392d397b25d37bfbec012cd84
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-06-13 17:48:14 +0900

    lecture04

---
+++ Lecture04/app/src/main/java/com/example/lecture04/Constraint.kt
@@ -0,0 +1,11 @@
+package com.example.lecture04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Constraint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
